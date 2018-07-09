9b43550e5847f827f4a19876678a4f3c49065e90
Aaron Ford <aaron@aaronford.net>
2018-07-09 20:09:26 +0000

    minor syntax changes

--- app.js
+++ app.js
@@ -2,13 +2,14 @@ var request = require('request');
 var fs = require('fs');
 
 request.get('https://sytantris.github.io/http-examples/future.jpg')
-       .on('error', function (err) {
-         throw err;
-       })
-       .on('response', function (response) {
-         console.log('Response received:', response.statusCode + ' ' + response.statusMessage + '\n' + 'Content type: ' + response.headers['content-type']);
-         console.log('Downloading image...');
-       }).on('end', function () {
-         console.log('Download complete.');
-       })
-       .pipe(fs.createWriteStream('./future.jpg'));
+  .on('error', function (err) {
+    throw err;
+  })
+  .on('response', function (response) {
+    console.log('Response received:', response.statusCode + ' ' + response.statusMessage + '\n' + 'Content type: ' + response.headers['content-type']);
+    console.log('Downloading image...');
+  })
+  .on('end', function () {
+    console.log('Download complete.');
+  })
+  .pipe(fs.createWriteStream('./future.jpg'));
